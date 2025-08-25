4d045f524014a4323211160738bc3a7bd437ddf7
Ariel Ramirez <arielrp2602@gmail.com>
2025-08-25 13:49:19 -0600

    fixes

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -2,7 +2,9 @@ import { Routes } from '@/common/enums';
 import { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
 import { redirect } from 'next/navigation';
 
-export function checkUser(user: KindeUser<Record<string, any>> | null) {
+export function checkUser(
+  user: KindeUser<Record<string, string | number | boolean>> | null
+) {
   if (!user) {
     return redirect(Routes.REGISTER);
   }
